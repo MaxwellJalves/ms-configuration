d4b1a5b5d0eb06398b0a84027fb59d81388a99f0
Maxwell Alves <maxwell880@gmail.com>
2022-06-07 00:23:56 -0300

    ms-configuration start

---
+++ src/main/java/com/max/msconfiguration/MsConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.max.msconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsConfigurationApplication.class, args);
+	}
+
+}
